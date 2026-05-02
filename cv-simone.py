31b954b7d52542a407137f9b5531f1ce629a65db
fersimone <sidesousfernan@gmail.com>
2026-05-02 18:18:06 +0200

    Update requirements

--- cv-simone.py
+++ cv-simone.py
@@ -137,5 +137,8 @@ def home():
     return render_template("index.html", cv=cv)
 
 
+#if __name__ == "__main__":
+#    app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
